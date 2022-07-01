e8cb0ad4af378c5c29ba6a671b2b2511ef98e227
Andreas Ulmer <andreas.ulmer@leanix.net>
2022-07-01 11:15:41 +0200

    HOOK-4306 add snapshot-transfer-cli

---
+++ snapshot-transfer-cli-installer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -euo pipefail
+
+# Obtain a static link to AzCopy: curl -s -D- https://aka.ms/downloadazcopy-v10-linux | grep ^Location
+AZCOPY_URL=https://azcopyvnext.azureedge.net/release20220315/azcopy_linux_amd64_10.14.1.tar.gz
+AZCOPY_VERSION=10.14.1
+
+echo "Starting to install AzCopy."
+curl --output /tmp/snapshot-transfer-cli-installer/azcopy.tar.gz --fail --silent --show-error --create-dirs $AZCOPY_URL
+tar -xf /tmp/snapshot-transfer-cli-installer/azcopy.tar.gz --directory /tmp/snapshot-transfer-cli-installer
+mv /tmp/snapshot-transfer-cli-installer/azcopy_linux_amd64_${AZCOPY_VERSION}/azcopy /usr/local/bin/.
+
+echo "Starting to install the snapshot-transfer-cli."
+mv /snapshot-transfer-cli /usr/local/bin/.
+
+echo "Cleaning up."
+rm -rf /tmp/snapshot-transfer-cli-installer
+
+echo "Installation finished."
